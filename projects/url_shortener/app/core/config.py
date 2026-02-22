0a8055aaf6804da00050bfc62e3379e9b1d647ff
Kanailal Manna <kanailalmanna2003@gmail.com>
2026-02-22 12:00:54 +0530

    Add Alembic configuration files and initial migration for URLs table

--- projects/url_shortener/app/core/config.py
+++ projects/url_shortener/app/core/config.py
@@ -1,7 +1,15 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
     DATABASE_URL: str
     BASE_URL: str = "http://localhost:8000"
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+
 
 settings = Settings()
